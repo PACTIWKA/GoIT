f855f04f551611fd3a174d1dfb980c272d3a2ba2
Vlad Krizanivski <volidolrtub@gmail.com>
2021-11-04 20:36:14 +0200

    for

--- Lessons/Lesson 3/Lesson 3.py
+++ Lessons/Lesson 3/Lesson 3.py
@@ -1,1 +1,27 @@
-pupils = ['Igor' , 'Vany']
+class_10a = ['Igor' , 'Vany' , 'leon'] #Class  
+for pupils in class_10a:
+    print(pupils)
+else:
+    print ('Всі учні')
+
+Message = 'Hi Dima' #Message Dima
+for char in Message:
+    print(char , end=' ')
+
+left = int(input('Left:'))
+right = int(input('Right:'))
+
+num = range(left , right) #Num for num range
+sum = 0 
+
+for q in num :
+    print(num)
+    
+
+numbers = input().split()  #Split 
+
+suma = 0 
+for sum in numbers:
+    suma += int(numbers)
+
+print(suma)
