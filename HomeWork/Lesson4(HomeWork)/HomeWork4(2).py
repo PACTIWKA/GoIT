9d2c54894f1045cf31ec4247776281e82f1e97a7
Vlad Krizanivski <volidolrtub@gmail.com>
2021-11-15 23:43:45 +0200

    HomeWork

---
+++ HomeWork/Lesson4(HomeWork)/HomeWork4(2).py
@@ -0,0 +1,13 @@
+Telephone = [
+ ['Phone' ,'Honor', 12100 ]
+ ['NoteBook' , 'Lenovo' , 10000]
+ ['NoteBook', 'Hp' , 12500]
+ ['Phone' , 'Samsung', 12300]
+]
+
+while True:
+    j = input('Введите что иммено?(Phone|NoteBook): ')
+    if j == 'Phone': 
+        print[0][3]
+    elif j == 'NoteBook':
+        print[1][2]
