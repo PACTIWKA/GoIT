98b53694c1953e55dadd4cd7cbd6518a8aa62f63
Vlad Krizanivski <volidolrtub@gmail.com>
2021-11-17 20:02:28 +0200

    I didn't know how?

--- HomeWork/Lesson4(HomeWork)/HomeWork4(2).py
+++ HomeWork/Lesson4(HomeWork)/HomeWork4(2).py
@@ -1,14 +1,21 @@
 Telephone = [
- ['Phone' ,'Honor', 12100 ]
- ['NoteBook' , 'Lenovo' , 10000]
- ['NoteBook', 'Hp' , 12500]
+ ['Phone' ,'Honor', 12100],
+ ['NoteBook' , 'Lenovo' , 10000],
+ ['NoteBook', 'Hp' , 12500],
  ['Phone' , 'Samsung', 12300]
 ]
 
-while True:
+for shop in Telephone:
     j = input('Введите что иммено?(Phone|NoteBook): ')
     if j == 'Phone': 
-        print[0][3]
+        print  (Telephone[0]) 
+        print  (Telephone[3])
+        break
     elif j == 'NoteBook':
-        print[1][2]
-       
+        print (Telephone[1]) 
+        print  (Telephone[2])
+        break
+for sum in shop:
+    c = int(input('Введите цену(макс): '))
+    if c <= shop[2]:
+        print (shop)
