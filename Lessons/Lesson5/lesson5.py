48fc0debec3e2035303da097c9a1441a3db7b71a
Vlad Krizanivski <volidolrtub@gmail.com>
2021-11-18 20:35:21 +0200

    Lesson5

---
+++ Lessons/Lesson5/lesson5.py
@@ -0,0 +1,96 @@
+'''
+pupils_count = 13                #str файл и как он работает
+s = str(pupils_count)
+print (s)
+print(type(s))
+
+s = str([12,11])
+print(s)
+
+print(str(10.2))
+'''
+
+'''
+mess = 'Hell Vlad'                     # plus 
+l = mess[2] + mess[4]
+print(l)
+'''
+
+'''
+message = 'Kiril'                    # minus
+message[-1] + message[-2]
+print(message[-1] + message[-2])
+'''
+
+'''
+l = """Yes but what ist,     #Fashon
+Yes but no,
+No but yes,"""
+print(l)
+'''
+
+'''
+pip = 'Vlad Krs'        # pip[0] == pip[-3]
+firs = pip[0]
+last = pip[-3]
+print (last)
+print (firs)
+
+if firs == last:
+    print('Сходятся')
+else:
+    print('Не а')
+'''
+
+'''
+s = 'asdfhueflefji'   #Slithe[:] устанавлюет рамки
+    #012345
+l = s[0:6]
+print(l)
+'''
+
+'''
+l = 'Jonaton'
+
+l[0] = 'RRR'
+'''
+
+'''
+L = 'Hi' + ' World' + ' $'
+print(L)
+'''
+
+'''
+m = '_' *25
+print(m)
+print('hi')
+'''
+
+'''
+fio = '1\tVlad\t Krz\n'
+print(fio)
+fio1 = '2\tVlad\t Krz\n'
+print(fio1)
+fio3 = '3\tVlad\t Krz\n'
+print(fio3)
+'''
+
+'''
+name = "д'Dimas'"
+print(name)
+
+cytata = "Автор сказав: \"Цитата\""
+print(cytata)
+len(name)
+'''
+'''
+slova_spama = ['buy' , 'sell' , 'reclama']
+'''
+
+'''
+name = "д'Dimas'"
+name_1 = 'ia'
+name.find('l')       #Проверка частями
+
+name.count(name_1)     #проверка всего
+'''
