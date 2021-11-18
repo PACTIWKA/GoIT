f356fd643ce0ab534ecf335cc3e2583943ff78c0
Vlad Krizanivski <volidolrtub@gmail.com>
2021-11-18 19:36:15 +0200

    recomplet

--- HomeWork/Lesson4(HomeWork)/HomeWork4(2).py
+++ HomeWork/Lesson4(HomeWork)/HomeWork4(2).py
@@ -1,22 +1,15 @@
-Telephone = [
- ['Phone' ,'Honor', 12100],
- ['NoteBook' , 'Lenovo' , 10000],
- ['NoteBook', 'Hp' , 12500],
- ['Phone' , 'Samsung', 12300]
+telephone = [
+ ['phone' ,'Honor', 12100],
+ ['notebook' , 'Lenovo' , 10000],
+ ['notebook', 'Hp' , 12500],
+ ['phone' , 'Samsung', 12300]
 ]
 
-for shop in Telephone:
-    j = input('Введите что иммено?(Phone|NoteBook): ')
-    if j == 'Phone': 
-        print  (Telephone[0]) 
-        print  (Telephone[3])
-        break
-    elif j == 'NoteBook':
-        print (Telephone[1]) 
-        print  (Telephone[2])
-        break
-for sum in shop:
-    k = int(input('Введите цену(мин): '))
-    c = int(input('Введите цену(макс): '))
-    if c and k <= shop[2]:
-        print (shop)
+j = input('Введите что иммено?(Phone|NoteBook): ')
+for type in telephone:
+  if j in type:
+    print(type)
+c = int(input('Введите цену: '))
+for type in telephone:
+  if type[2] < c :
+    print(type)
